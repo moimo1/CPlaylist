7c716c0085cd5503875928f8055c711d4185383b
Renz Dadpaas <rdadpaas123@gmail.com>
2026-03-13 20:39:37 +0800

    defined song

--- playlist.h
+++ playlist.h
@@ -2,4 +2,12 @@
 #ifndef PLAYLISTAPP_PLAYLIST_H
 #define PLAYLISTAPP_PLAYLIST_H
 
+#include "raylib.h"
+
+typedef struct Song {
+    char title[100];
+    char artist[100];
+    char album[100];
+    Music music;
+} Song;
 #endif //PLAYLISTAPP_PLAYLIST_H
