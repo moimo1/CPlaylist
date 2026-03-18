51705c4ff43e4561f2b7fa422f285df318487793
Renz Dadpaas <rdadpaas123@gmail.com>
2026-03-18 11:48:34 +0800

    implemented createPlaylist function

--- playlist.c
+++ playlist.c
@@ -1,1 +1,13 @@
 #include "playlist.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+void createPlaylist(Playlist* pl, const char* title) {
+    strncpy(pl->title, title, sizeof(pl->title));
+
+    pl->head = NULL;
+    pl->tail = NULL;
+    pl->current = NULL;
+    pl->count = 0;
+}
